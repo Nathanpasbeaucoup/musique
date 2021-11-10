515b0f4f331b2b294e8ffe0b6185a7117f6d2a86
Nathanpasbeaucoup <92890159+Nathanpasbeaucoup@users.noreply.github.com>
2021-11-10 10:43:47 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,22 +2,22 @@ input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 3; index++) {
         music.setTempo(120)
         music.playTone(294, music.beat(BeatFraction.Half))
-        basic.pause(70)
+        basic.pause(57)
         music.playTone(330, music.beat(BeatFraction.Half))
-        basic.pause(70)
+        basic.pause(57)
         music.playTone(392, music.beat(BeatFraction.Half))
-        basic.pause(70)
+        basic.pause(57)
         for (let index = 0; index < 5; index++) {
             music.playTone(392, music.beat(BeatFraction.Half))
-            basic.pause(70)
+            basic.pause(30)
         }
     }
     music.playTone(392, music.beat(BeatFraction.Half))
-    basic.pause(70)
+    basic.pause(57)
     music.playTone(392, music.beat(BeatFraction.Half))
-    basic.pause(70)
+    basic.pause(57)
     music.playTone(370, music.beat(BeatFraction.Half))
-    basic.pause(70)
+    basic.pause(57)
     music.rest(music.beat(BeatFraction.Whole))
 })
 basic.forever(function () {
